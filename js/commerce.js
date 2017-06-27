d26a5c59758cfcbb2b6a4876a730457ae5fc96e6
mrjcm <461679353@qq.com>
2017-06-27 15:24:49 +0800

    测试页面性能优化

--- js/commerce.js
+++ js/commerce.js
@@ -1,19 +1,5 @@
 
 $(function(){
-	/*轮播*/
-	var bdwidth=$(window).width();
-	if (bdwidth<1200) {
-		var wid =document.documentElement.clientWidth;
-			var scale = wid/1440;
-			document.body.style.zoom = scale;
-			window.onresize = function(){
-			var wid =document.documentElement.clientWidth;
-			var scale = wid/1440;
-			document.body.style.zoom = scale;
-		}
-	}
-
-	// 轮播结束
 			$(".indexPublic").mouseover(function(){
 				$(this).animate({marginTop:0},100);
 			})
