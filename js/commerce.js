df296b220fb7f693cb52d77150221cc4c9afbad9
mrjcm <461679353@qq.com>
2017-06-27 15:23:51 +0800

    测试页面性能

--- js/commerce.js
+++ js/commerce.js
@@ -1,10 +1,23 @@
 
 $(function(){
-	// 新闻 资源 事件的动效
-	$(".indexPublic").mouseover(function(){
-		$(this).animate({marginTop:0},100);
-	})
-	$(".indexPublic").mouseleave(function(){
-		$(this).animate({marginTop:30},100);
-	})
+	/*轮播*/
+	var bdwidth=$(window).width();
+	if (bdwidth<1200) {
+		var wid =document.documentElement.clientWidth;
+			var scale = wid/1440;
+			document.body.style.zoom = scale;
+			window.onresize = function(){
+			var wid =document.documentElement.clientWidth;
+			var scale = wid/1440;
+			document.body.style.zoom = scale;
+		}
+	}
+
+	// 轮播结束
+			$(".indexPublic").mouseover(function(){
+				$(this).animate({marginTop:0},100);
+			})
+			$(".indexPublic").mouseleave(function(){
+				$(this).animate({marginTop:30},100);
+			})
 })
